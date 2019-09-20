c491c4b56345315766154352fb19bae0e0abe643
tyfa7215 <tyfa7215@colorado.edu>
2019-09-20 11:41:50 -0600

    added updated problem 4 w/ email list

---
+++ lab3_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Authors : Tyler Faragallah
+# Date: 09/20/2019
+
+#Problem 1 Code:
+##Searching for a regex
+echo "enter a file name" #ask for filename
+read fileName
+echo "enter a regular expression" #ask for regular expression
+read regExpress
+grep -E $regExpress $fileName #search for regular expression
+
+##Counting phone numbers
+grep -E -c "[0-9]{3}-[0-9]{3}-[0-9]{4}" $fileName
+
+##Counting Emails
+grep -E -c "[A-Za-z0-9]*@[A-Za-z0-9]*.[A-Za-z0-9]{3}" $fileName
+
+##Writing emails to file
+grep -E "[A-Za-z0-9]*@geocities.com" $fileName >> email_results.txt
